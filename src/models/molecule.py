130a8bd9f6ee00962773d8a3b01adb161ad68ad3
test <alinaluzanova@gmail.com>
2024-01-15 01:30:32 +0300

    add rdkit

--- src/models/molecule.py
+++ src/models/molecule.py
@@ -7,4 +7,12 @@ class Molecule:
     def to_dict(self):
         return {"name": self.name, "smiles": self.smiles}
 
+    def create_node(self, tx):
+        query = (
+            "CREATE (m:Molecule {name: $name, smiles: $smiles}) "
+            "RETURN m"
+        )
+        result = tx.run(query, **self.to_dict())
+        return result.single()[0]
+
 
--- src/models/reaction.py
+++ src/models/reaction.py
@@ -4,3 +4,11 @@ class Reaction:
 
     def to_dict(self):
         return {"name": self.name}
+
+    def create_node(self, tx):
+        query = (
+            "CREATE (r:Reaction {name: $name}) "
+            "RETURN r"
+        )
+        result = tx.run(query, **self.to_dict())
+        return result.single()[0]
